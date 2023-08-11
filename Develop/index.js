2d433058ef9fad82b992a314e5b0ee486e94dd81
Noah Burton <nburton115@gmail.com>
2023-08-11 13:15:09 -0600

    inquirer prompt for title

--- Develop/index.js
+++ Develop/index.js
@@ -14,7 +14,32 @@ const log = console.log;
 
 function init() {
     const welcomeTitle = log(chalk.bgCyanBright('All aboard the READ.ME Express \n Complete the following prompts to generate your READ.ME: \n'));
-}
+};
 
 // Function call to initialize app
 init();
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your Project?',
+            name: 'title',
+        },
+    ])
+    .then((answers) => {
+        console.log(answers);
+         const title = answers.title;
+         console.log(title);
+        // .split(' ').join('').json
+        
+    })
+    // .catch((error) => {
+    //     if (error.isTtyError) {
+    //         console.log('Tty error')
+    //       // Prompt couldn't be rendered in the current environment
+    //     } else {
+    //         console.log('error msg')
+    //       // Something else went wrong
+    //     }
+    //   });
