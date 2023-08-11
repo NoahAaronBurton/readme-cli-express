f72bf92875e9f2dcda9c6816dfe0d6f3d9bdc663
Noah Burton <nburton115@gmail.com>
2023-08-11 11:07:01 -0600

    welcome title and chalk working

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,6 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
@@ -7,7 +10,11 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const log = console.log;
+
+function init() {
+    const welcomeTitle = log(chalk.bgCyanBright('All aboard the READ.ME Express \n Complete the following prompts to generate your READ.ME: \n'));
+}
 
 // Function call to initialize app
 init();
